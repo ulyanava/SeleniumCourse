2f20daaec606cfcb0da5481896b12a6a27d747ae
ulyanava <taniauljanova@gmail.com>
2017-04-30 13:28:12 +0300

    OpenBrowser Hometask1 added

---
+++ Hometask/src/test/java/HomeTask1/OpenBrowser.java
@@ -0,0 +1,33 @@
+package HomeTask1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * HomeTask1: Start Browser, Open Google.com and Close Browser
+ */
+public class OpenBrowser {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait (driver, 10);
+    }
+    @Test
+    public void openBrowser() {
+        driver.navigate().to("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @After
+    public void stop(){
+
+    }
+}
