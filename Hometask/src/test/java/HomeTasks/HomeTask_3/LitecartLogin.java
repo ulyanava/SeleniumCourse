dca3f7e25ab984ec99badeb1b8dfbb389490870f
ulyanava <taniauljanova@gmail.com>
2017-05-01 19:25:04 +0300

    Login method refactored

--- Hometask/src/test/java/HomeTasks/HomeTask_3/LitecartLogin.java
+++ Hometask/src/test/java/HomeTasks/HomeTask_3/LitecartLogin.java
@@ -27,15 +27,19 @@ public class LitecartLogin {
   public void openBrowser() {
     driver.get("http://localhost/litecart/admin");
     driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-    driver.findElement(By.name("username")).sendKeys("admin");
-    driver.findElement(By.name("password")).sendKeys("admin");
+    login("admin", "admin");
+  }
+
+  private void login(String username, String password) {
+    driver.findElement(By.name("username")).sendKeys(username);
+    driver.findElement(By.name("password")).sendKeys(password);
     driver.findElement(By.xpath("*//button[@name='login']")).click();
     driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
   }
 
   @After
   //logout and stop
-    public void stop() {
+  public void stop() {
     driver.findElement(By.xpath("//*[@id=\"shortcuts\"]/a[5]/i")).click();
     driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
     driver.quit();
