1814e9de0188f84e840b325022115fc46a8ccc23
ulyanava <taniauljanova@gmail.com>
2017-05-01 16:27:52 +0300

    Rename class and package

--- Hometask/src/test/java/HomeTasks/HomeTask1.java
+++ Hometask/src/test/java/HomeTasks/HomeTask1.java
@@ -1,4 +1,4 @@
-package HomeTask1;
+package HomeTasks;
 
 import org.junit.After;
 import org.junit.Before;
@@ -7,28 +7,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.util.concurrent.TimeUnit;
 
 /**
  * HomeTask1: Start Browser, Open Google.com and Close Browser
  */
-public class OpenBrowser {
+public class HomeTask1 {
     private WebDriver driver;
     private WebDriverWait wait;
 
     @Before
-    public void start(){
+    public void start() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait (driver, 10);
+        wait = new WebDriverWait(driver, 10);
     }
+
     @Test
     public void openBrowser() {
         driver.navigate().to("http://www.google.com");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
     @After
-    public void stop(){
+    public void stop() {
         driver.quit();
     }
 }
