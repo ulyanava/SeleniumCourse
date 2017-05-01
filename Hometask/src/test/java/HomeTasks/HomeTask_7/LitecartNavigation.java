16d1907805029b86f2941b2d635ed9349f0fdb9a
ulyanava <taniauljanova@gmail.com>
2017-05-01 19:29:04 +0300

    HomeTask 7 initial

---
+++ Hometask/src/test/java/HomeTasks/HomeTask_7/LitecartNavigation.java
@@ -0,0 +1,48 @@
+package HomeTasks.HomeTask_7;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by tatianka on 01.05.2017.
+ */
+public class LitecartNavigation {
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  @Test
+  public void openBrowser() {
+    driver.get("http://localhost/litecart/admin");
+    driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+    login("admin", "admin");
+  }
+
+  private void login(String username, String password) {
+    driver.findElement(By.name("username")).sendKeys(username);
+    driver.findElement(By.name("password")).sendKeys(password);
+    driver.findElement(By.xpath("*//button[@name='login']")).click();
+    driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+  }
+
+  @After
+  //logout and stop
+  public void stop() {
+    driver.findElement(By.xpath("//*[@id=\"shortcuts\"]/a[5]/i")).click();
+    driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+    driver.quit();
+  }
+
+}
