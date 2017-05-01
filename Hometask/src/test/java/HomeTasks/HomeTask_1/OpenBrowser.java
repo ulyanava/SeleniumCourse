4e55e56d09cc6bfc9118c712f1694d23f67800f8
ulyanava <taniauljanova@gmail.com>
2017-05-01 19:09:31 +0300

    HomeTask 2 added (works good)

--- Hometask/src/test/java/HomeTasks/HomeTask_1/OpenBrowser.java
+++ Hometask/src/test/java/HomeTasks/HomeTask_1/OpenBrowser.java
@@ -13,23 +13,23 @@ import java.util.concurrent.TimeUnit;
  * HomeTask1: Start Browser, Open Google.com and Close Browser
  */
 public class OpenBrowser {
-    private WebDriver driver;
-    private WebDriverWait wait;
+  private WebDriver driver;
+  private WebDriverWait wait;
 
-    @Before
-    public void start() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-    }
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
 
-    @Test
-    public void openBrowser() {
-        driver.navigate().to("http://www.google.com");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
+  @Test
+  public void openBrowser() {
+    driver.navigate().to("http://www.google.com");
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
 
-    @After
-    public void stop() {
-        driver.quit();
-    }
+  @After
+  public void stop() {
+    driver.quit();
+  }
 }
---
+++ Hometask/src/test/java/HomeTasks/HomeTask_3/LitecartLogin.java
@@ -0,0 +1,43 @@
+package HomeTasks.HomeTask_3;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Login to litecart admin console and logout
+ */
+public class LitecartLogin {
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  @Test
+  public void openBrowser() {
+    driver.get("http://localhost/litecart/admin");
+    driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.xpath("*//button[@name='login']")).click();
+    driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+  }
+
+  @After
+  //logout and stop
+    public void stop() {
+    driver.findElement(By.xpath("//*[@id=\"shortcuts\"]/a[5]/i")).click();
+    driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+    driver.quit();
+  }
+}
