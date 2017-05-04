90077a1e21d1ec3784d3d296ed24e02841b5f191
ulyanava <taniauljanova@gmail.com>
2017-05-04 20:46:47 +0300

    LiteCartNavogation added
    all clicks works good

--- Hometask/src/test/java/HomeTasks/HomeTask_7/LitecartNavigation.java
+++ Hometask/src/test/java/HomeTasks/HomeTask_7/LitecartNavigation.java
@@ -23,11 +23,13 @@ public class LitecartNavigation {
     wait = new WebDriverWait(driver, 10);
   }
 
+
   @Test
-  public void openBrowser() {
+  public void menuCheckTest() {
     driver.get("http://localhost/litecart/admin");
     driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
     login("admin", "admin");
+    menuClicker();
   }
 
   private void login(String username, String password) {
@@ -35,6 +37,102 @@ public class LitecartNavigation {
     driver.findElement(By.name("password")).sendKeys(password);
     driver.findElement(By.xpath("*//button[@name='login']")).click();
     driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+  }
+
+  private void menuClicker() {
+    //Appearance
+    driver.findElement(By.cssSelector(".list-vertical li:first-child")).click();
+    driver.findElement(By.cssSelector("#doc-template span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-logotype span[class=name]")).click();
+
+    //Catalog
+    driver.findElement(By.cssSelector(".list-vertical a[href *=catalog]")).click();
+    driver.findElement(By.cssSelector("#doc-catalog span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-product_groups span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-option_groups span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-manufacturers span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-suppliers span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-delivery_statuses span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-sold_out_statuses span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-quantity_units span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-csv span[class=name]")).click();
+
+    //Countries
+    driver.findElement(By.cssSelector(".list-vertical a[href *=countries]")).click();
+
+    //Currencies
+    driver.findElement(By.cssSelector(".list-vertical a[href *=currencies]")).click();
+
+    //Customers
+    driver.findElement(By.cssSelector(".list-vertical a[href *=customers]")).click();
+    driver.findElement(By.cssSelector("#doc-customers span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-csv span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-newsletter span[class=name]")).click();
+
+    //Geo Zones
+    driver.findElement(By.cssSelector(".list-vertical a[href *=geo_zones]")).click();
+
+    //Languages
+    driver.findElement(By.cssSelector(".list-vertical a[href *=languages]")).click();
+    driver.findElement(By.cssSelector("#doc-languages span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-storage_encoding span[class=name]")).click();
+
+    //Modules
+    driver.findElement(By.cssSelector(".list-vertical a[href *=modules]")).click();
+    driver.findElement(By.cssSelector("#doc-customer span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-shipping span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-payment span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-order span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-order_total span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-jobs span[class=name]")).click();
+
+    //Orders
+    driver.findElement(By.cssSelector(".list-vertical a[href *=orders]")).click();
+    driver.findElement(By.cssSelector("#doc-orders span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-order_statuses span[class=name]")).click();
+
+    //Pages
+    driver.findElement(By.cssSelector(".list-vertical a[href *=pages]")).click();
+    driver.findElement(By.cssSelector("#doc-pages span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-csv span[class=name]")).click();
+
+    //Reports
+    driver.findElement(By.cssSelector(".list-vertical a[href *=reports]")).click();
+    driver.findElement(By.cssSelector("#doc-monthly_sales span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-most_sold_products span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-most_shopping_customers span[class=name]")).click();
+
+    //Settings
+    driver.findElement(By.cssSelector(".list-vertical a[href *=settings]")).click();
+    driver.findElement(By.cssSelector("#doc-store_info span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-defaults span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-general span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-listings span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-images span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-checkout span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-advanced span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-security span[class=name]")).click();
+
+    //Slides*
+    driver.findElement(By.cssSelector(".list-vertical a[href *=slides]")).click();
+
+    //Tax*
+    driver.findElement(By.cssSelector(".list-vertical a[href *=tax]")).click();
+    driver.findElement(By.cssSelector("#doc-tax_rates span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-tax_classes span[class=name]")).click();
+
+    //Translations*
+    driver.findElement(By.cssSelector(".list-vertical a[href *=translations]")).click();
+    driver.findElement(By.cssSelector("#doc-search span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-scan span[class=name]")).click();
+    driver.findElement(By.cssSelector("#doc-csv span[class=name]")).click();
+
+    //Users*
+    driver.findElement(By.cssSelector(".list-vertical a[href *=users]")).click();
+
+    //Vqmods*
+    driver.findElement(By.cssSelector(".list-vertical a[href *=vqmods]")).click();
+
   }
 
   @After
@@ -46,3 +144,4 @@ public class LitecartNavigation {
   }
 
 }
+
