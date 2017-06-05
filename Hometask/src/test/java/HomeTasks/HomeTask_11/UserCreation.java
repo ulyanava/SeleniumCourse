8b79661d6bce401c9be9b7d098375b913dbba859
ulyanava <taniauljanova@gmail.com>
2017-06-05 08:10:32 +0300

    Task 11 Draft
    failed with email and drop down

---
+++ Hometask/src/test/java/HomeTasks/HomeTask_11/UserCreation.java
@@ -0,0 +1,123 @@
+package HomeTasks.HomeTask_11;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Date;
+
+public class UserCreation {
+  private WebDriver driver;
+  private WebDriverWait wait;
+  //CreateUserForm user = new CreateUserForm();
+
+  String taxID = "12345";
+  String firstName = "Jason";
+  String lastName = "Momoa";
+  String address1 = "California";
+  String postcode = "22014";
+  String country = "United States";
+  String email = "System.currentTimeMillis()" + "@gmail.com";
+  String password = "12345";
+  String company = "Epam";
+  String address2 = "New York";
+  String city = "Minsk";
+  String zone = "New York";
+  String phone = "2608980";
+
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+    driver.get("http://localhost/litecart/en/create_account");
+
+  }
+
+  @Test
+  public void createUser() {
+    fillTaxID(taxID);
+    fillCompany(company);
+    fillFirstName(firstName);
+    fillLastName(lastName);
+    fillAddress1(address1);
+    fillAddress2(address2);
+    fillPostcode(postcode);
+    //fillCountry(country);
+    fillEmail(email);
+    fillPassword(password);
+    fillConfirmPassword(password);
+    fillCity(city);
+ //   fillZone(zone);
+    fillPhone(phone);
+    //submitCreateAcc();
+   // logout();
+  }
+
+  public void fillTaxID(String taxID) {
+    driver.findElement(By.cssSelector("input[name=tax_id]")).sendKeys(taxID);
+  }
+
+  public void fillCompany(String company) {
+    driver.findElement(By.cssSelector("input[name=company]")).sendKeys(company);
+  }
+
+  public void fillFirstName(String firstName) {
+    driver.findElement(By.cssSelector("input[name=firstname]")).sendKeys(firstName);
+  }
+
+  public void fillLastName(String lastName) {
+    driver.findElement(By.cssSelector("input[name=lastname]")).sendKeys(lastName);
+  }
+
+  public void fillAddress1(String address1) {
+    driver.findElement(By.cssSelector("input[name=address1]")).sendKeys(address1);
+  }
+
+  public void fillAddress2(String address2) {
+    driver.findElement(By.cssSelector("input[name=address2]")).sendKeys(address2);
+  }
+
+  public void fillPostcode(String postcode) {
+    driver.findElement(By.cssSelector("input[name=postcode]")).sendKeys(postcode);
+  }
+
+  public void fillCountry(String country) {
+    driver.findElement(By.cssSelector("input[name=country_code]")).sendKeys(country);
+  }
+
+  public void fillEmail(String email) {
+    driver.findElement(By.cssSelector("input[name=email]")).sendKeys(email);
+  }
+
+  public void fillPassword(String password) {
+    driver.findElement(By.cssSelector("input[name=password]")).sendKeys(password);
+  }
+
+  public void fillConfirmPassword(String password) {
+    driver.findElement(By.cssSelector("input[name=confirmed_password]")).sendKeys(password);
+  }
+
+  public void fillCity(String city) {
+    driver.findElement(By.cssSelector("input[name=city]")).sendKeys(city);
+  }
+
+ /* public void fillZone(String zone) {
+    driver.findElement(By.cssSelector("input[name=zone_code]")).sendKeys(zone);
+  }*/
+
+  public void fillPhone(String phone) {
+    driver.findElement(By.cssSelector("input[name=phone]")).sendKeys(phone);
+  }
+  public void submitCreateAcc() {
+    driver.findElement(By.cssSelector("input[name=create_account]")).click();
+  }
+  public void logout() {
+    driver.findElement(By.cssSelector("#box-account ul [href *=logout]")).click();
+
+  }
+
+}
