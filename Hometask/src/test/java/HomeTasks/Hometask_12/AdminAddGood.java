a8803cf448e39d5e34e03911130effcec4d9a892
ulyanava <taniauljanova@gmail.com>
2017-06-10 23:19:21 +0300

    HomeTask12 done works

--- Hometask/src/test/java/HomeTasks/Hometask_12/AdminAddGood.java
+++ Hometask/src/test/java/HomeTasks/Hometask_12/AdminAddGood.java
@@ -4,12 +4,13 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.NoSuchElementException;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class AdminAddGood {
@@ -36,117 +37,152 @@ public class AdminAddGood {
   public void addGood() {
     clickCatalog();
     clickAddNewProduct();
-    //Not done
-    //fillGeneral();
-   // openInformation();
-    //Fix Description!!
-    //fillInformation();
+    String name = "name_" + System.currentTimeMillis();
+    fillGeneral(name);
+    openInformation();
+    fillInformation();
     openPrices();
     fillPrices();
+    save();
+    clickCatalogCatalog();
+    String pathGood = String.format("//a[text()='%s']", name);
+    System.out.println(pathGood);
+    Assert.assertTrue(name, driver.findElement(By.xpath(pathGood)).isDisplayed());
+
+
   }
 
+  //Navigation
   private void clickCatalog() {
     driver.findElement(By.cssSelector("ul#box-apps-menu > li:nth-child(2)")).click();
   }
-
-  private void clickAddNewProduct() {
-    driver.findElement(By.cssSelector("ul.list-inline a[href*='edit_product']")).click();
-  }
-
-  private void fillGeneral() {
-    //*code*
-    driver.findElement(By.cssSelector(".tab-content input[name='code']")).sendKeys("123456");
-    //*name*
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-
-   //??Categories checkbox
-
-    //Default Category selectBox
-    Select select= new Select(driver.findElement(By.cssSelector("#tab-general select[name='default_category_id']")));
-    select.selectByIndex(0);
-
-
-    /*//???Date Valid From
-    ((JavascriptExecutor)driver).executeScript ("document.getElementById('fromDate').removeAttribute('readonly',0);"); // Enables the from date box
-
-    WebElement fromDateBox= driver.findElement(By.id("fromDate"));
-    fromDateBox.clear();
-    fromDateBox.sendKeys("8-Dec-2014"); //Enter date in required format*/
-
-//??Date Valid To
-//*SKU input*
-    driver.findElement(By.cssSelector("#tab-general input[name='sku']")).sendKeys("SKU");
-    //*GTIN input*
-    driver.findElement(By.cssSelector("#tab-general input[name='gtin']")).sendKeys("GTIN");
-    //*TARIC input*
-    driver.findElement(By.cssSelector("#tab-general input[name='taric']")).sendKeys("TARIC");
-//???Quantity rows
-    //???Weight
-    //???Width x Height x Length
-
-    //*Delivery Status select*
-    select= new Select(driver.findElement(By.cssSelector("#tab-general select[name='delivery_status_id']")));
-    select.selectByIndex(0);
-
-    //*Sold Out Status select*
-    select= new Select(driver.findElement(By.cssSelector("#tab-general select[name='sold_out_status_id']")));
-    select.selectByIndex(0);
-  }
-
   private void openInformation() {
     driver.findElement(By.cssSelector(".nav a[href='#tab-information']")).click();
   }
   private void openPrices() {
     driver.findElement(By.cssSelector(".nav a[href='#tab-prices']")).click();
   }
+  private void clickAddNewProduct() {
+    driver.findElement(By.cssSelector("ul.list-inline a[href*='edit_product']")).click();
+  }
+  private void clickCatalogCatalog() {
+    driver.findElement(By.cssSelector("a[href='http://localhost/litecart/admin/?app=catalog&doc=catalog']")).click();
+  }
+
+//General tab
+
+  private void fillGeneral(String name) {
+    //Input fields
+    String code = "code_" + System.currentTimeMillis();
+    driver.findElement(By.cssSelector("input[name='code']")).sendKeys(code);
+    driver.findElement(By.cssSelector("input[name='name[en]']")).sendKeys(name);
+    String skuName = "skuName_" + System.currentTimeMillis();
+    String gtinName = "gtinName_" + System.currentTimeMillis();
+    String taricName = "taricName_" + System.currentTimeMillis();
+    driver.findElement(By.cssSelector("input[name='sku']")).sendKeys(skuName);
+    driver.findElement(By.cssSelector("input[name='gtin']")).sendKeys(gtinName);
+    driver.findElement(By.cssSelector("input[name='taric']")).sendKeys(taricName);
+
+    //Button Status
+    driver.findElement(By.xpath("//div[@id='tab-general']//label[contains(text(),'Enabled')]")).click();
+
+    //Select drop downs
+    //Default Category selectBox
+    Select select = new Select(driver.findElement(By.cssSelector("select[name='default_category_id']")));
+    select.selectByIndex(0);
+    //*Delivery Status select*
+    select = new Select(driver.findElement(By.cssSelector("select[name='delivery_status_id']")));
+    select.selectByIndex(0);
+    //*Sold Out Status select*
+    select = new Select(driver.findElement(By.cssSelector("select[name='sold_out_status_id']")));
+    select.selectByIndex(0);
+
+//Check whether checkbox is selected and click if not
+    //*Categories checkbox*
+
+    if (driver.findElement(By.cssSelector("div.checkbox:nth-child(1) input")).isSelected()) ;
+    else
+      driver.findElement(By.cssSelector("div.checkbox:nth-child(1) input")).click();
+
+
+//Date Fields
+    driver.findElement(By.cssSelector("input[name='date_valid_from']")).sendKeys("01-01-2017");
+    driver.findElement(By.cssSelector("input[name='date_valid_to']")).sendKeys("10-06-2017");
+
+
+    //Input + select fields (clear mask+ type + select)
+//*Quantity rows*
+    driver.findElement(By.cssSelector("input[name='quantity']")).clear();
+    driver.findElement(By.cssSelector("input[name='quantity']")).sendKeys("2");
+    select = new Select(driver.findElement(By.cssSelector("select[name='quantity_unit_id']")));
+    select.selectByIndex(0);
+    //Weight*
+    driver.findElement(By.cssSelector("input[name='weight']")).clear();
+    driver.findElement(By.cssSelector("input[name='weight']")).sendKeys("1000");
+    select = new Select(driver.findElement(By.cssSelector("select[name='weight_class']")));
+    select.selectByValue("oz");
+    //*Width x Height x Length*
+    driver.findElement(By.cssSelector("input[name='dim_x']")).clear();
+    driver.findElement(By.cssSelector("input[name='dim_x']")).sendKeys("101");
+    driver.findElement(By.cssSelector("input[name='dim_y']")).clear();
+    driver.findElement(By.cssSelector("input[name='dim_y']")).sendKeys("102");
+    driver.findElement(By.cssSelector("input[name='dim_z']")).clear();
+    driver.findElement(By.cssSelector("input[name='dim_z']")).sendKeys("103");
+    select = new Select(driver.findElement(By.cssSelector("select[name='dim_class']")));
+    select.selectByValue("in");
+
+//Upload File
+    String path = new File("src/test/java/HomeTasks/Hometask_12/good.jpg").getAbsolutePath();
+    driver.findElement(By.cssSelector("input[name='new_images[]']")).sendKeys(path);
+
+  }
 
   private void fillInformation() {
     //*Manufacturer*
-    Select select = new Select(driver.findElement(By.cssSelector("#tab-information select[name='manufacturer_id']")));
+    Select select = new Select(driver.findElement(By.cssSelector("select[name='manufacturer_id']")));
     select.selectByIndex(0);
 //*Supplier*
-    select = new Select(driver.findElement(By.cssSelector("#tab-information select[name='supplier_id']")));
+    select = new Select(driver.findElement(By.cssSelector("select[name='supplier_id']")));
     select.selectByIndex(0);
     //*Keys*
-    driver.findElement(By.cssSelector("#tab-information input[name='keywords']")).sendKeys("keys");
+    driver.findElement(By.cssSelector("input[name='keywords']")).sendKeys("keys");
     //*short description*
-    driver.findElement(By.cssSelector("#tab-information input[name='short_description[en]']")).sendKeys("short_description");
+    driver.findElement(By.cssSelector("input[name='short_description[en]']")).sendKeys("short_description");
 
-//!!!!Локатор неверный, атрибут искать Description
-    driver.findElement(By.cssSelector("#tab-information textarea[name='description[en]']")).sendKeys("Description");
+//Description
+    driver.findElement(By.cssSelector("div.trumbowyg-editor")).sendKeys("Description");
+
     //*Attributes*
-    driver.findElement(By.cssSelector("#tab-information textarea[name='attributes[en]']")).sendKeys("Attributes");
+    driver.findElement(By.cssSelector("textarea[name='attributes[en]']")).sendKeys("Attributes");
     //*Head Title*
-    driver.findElement(By.cssSelector("#tab-information input[name='head_title[en]']")).sendKeys("head title");
+    driver.findElement(By.cssSelector("input[name='head_title[en]']")).sendKeys("head title");
     //*Meta Description*
-    driver.findElement(By.cssSelector("#tab-information input[name='meta_description[en]']")).sendKeys("meta_description");
+    driver.findElement(By.cssSelector("input[name='meta_description[en]']")).sendKeys("meta_description");
   }
 
   private void fillPrices() {
-    //Purchase Price
-    driver.findElement(By.cssSelector("#prices select[name='purchase_price_currency_code']")).click();
-    Select select = new Select(driver.findElement(By.cssSelector("#prices select[name='purchase_price_currency_code']")));
+    //*Purchase Price
+    driver.findElement(By.cssSelector("input[name='purchase_price']")).clear();
+    driver.findElement(By.cssSelector("input[name='purchase_price']")).sendKeys("200");
+    Select select = new Select(driver.findElement(By.cssSelector("select[name='purchase_price_currency_code']")));
     select.selectByIndex(2);
 
-    //Tax Class
+    //*Tax Class
     select = new Select(driver.findElement(By.cssSelector("#prices select[name='tax_class_id']")));
     select.selectByIndex(0);
 
-    //Price Incl. Tax (?)
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
+    //*Price Incl. Tax*
+    driver.findElement(By.cssSelector("input[name='gross_prices[USD]']")).clear();
+    driver.findElement(By.cssSelector("input[name='gross_prices[USD]']")).sendKeys("901");
+    driver.findElement(By.cssSelector("input[name='gross_prices[EUR]']")).sendKeys("902");
 
-
-    //Campaigns
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
-    driver.findElement(By.cssSelector(".tab-content input[name='name[en]']")).sendKeys("name");
   }
 
-/*
+  private void save() {
+    driver.findElement(By.cssSelector("button[name='save']")).click();
+  }
+
+
 @After
   //logout and stop
   public void stop() {
@@ -154,5 +190,5 @@ public class AdminAddGood {
     driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
     driver.quit();
   }
-  */
+
 }
